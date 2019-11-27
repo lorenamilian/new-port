ed311c7ac9a7a38136ecaa6e4a742d4b300392b9
lorena <lorenalies2312@gmail.com>
2019-11-27 15:42:35 -0500

    adding the process.env.PORT to the listen event

--- portfolio.js
+++ portfolio.js
@@ -21,6 +21,6 @@ app.get("/projects", function(request, response) {
 });
 
 //
-app.listen(5000, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log("server is running on port 5000");
 });
