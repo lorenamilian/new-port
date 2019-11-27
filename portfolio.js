7b06a80884844e4be97d33ad170ac68e51937c30
lorena <lorenalies2312@gmail.com>
2019-11-27 15:19:49 -0500

    adding nodemon

--- portfolio.js
+++ portfolio.js
@@ -22,5 +22,5 @@ app.get("/projects", function(request, response) {
 
 //
 app.listen(5000, function() {
-  console.log("server is running on port 3000");
+  console.log("server is running on port 5000");
 });
